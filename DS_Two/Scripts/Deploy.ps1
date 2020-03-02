7f24829b733bbf482809889aea7577c0d78175a5
ravi.kanneganti <ravi.kanneganti@datascan.com>
2020-03-02 17:00:23 -0600

    path change dac.dll

--- DS_Two/Scripts/Deploy.ps1
+++ DS_Two/Scripts/Deploy.ps1
@@ -2,7 +2,7 @@
 param([string]$tcdir="")
 
 # Load DAC.dll
-add-type -path "C:\Program Files (x86)\Microsoft SQL Server\110\DAC\bin\Microsoft.SqlServer.Dac.dll"
+add-type -path "C:\Program Files (x86)\Microsoft SQL Server\140\DAC\bin\Microsoft.SqlServer.Dac.dll"
 
 # Load SQL Server Instance
 $dacService = new-object Microsoft.SqlServer.Dac.DacServices "server=redgateprod.dart.local"
