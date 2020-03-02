8aa76fc7c86ba7d6600592830337eea0ed356d52
ravi.kanneganti <ravi.kanneganti@datascan.com>
2020-03-02 16:36:47 -0600

    Add project files.

---
+++ DS_Two/dbo/Tables/Blocking_Sessions.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[Blocking_Sessions] (
+    [session_id]            SMALLINT       NOT NULL,
+    [host_name]             NVARCHAR (128) NULL,
+    [login_name]            NVARCHAR (128) NULL,
+    [program_name]          NVARCHAR (128) NULL,
+    [start_time]            DATETIME       NOT NULL,
+    [blocking_session_id]   SMALLINT       NULL,
+    [Blocking_hostname]     NVARCHAR (128) NULL,
+    [Blocking_login_name]   NVARCHAR (128) NULL,
+    [Primary_SQL]           NVARCHAR (MAX) NULL,
+    [Blocking_SQL]          NVARCHAR (MAX) NULL,
+    [Created_Time]          DATETIME       NOT NULL,
+    [Blocking_program_name] NVARCHAR (128) NULL,
+    [Blocked_DBname]        NVARCHAR (128) NULL,
+    [Blocking_DBname]       NVARCHAR (128) NULL
+);
+
---
+++ DS_Two/dbo/Tables/Log_Shrink_Stats.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Log_Shrink_Stats] (
+    [databasename]  [sysname]      NULL,
+    [command]       VARCHAR (2000) NULL,
+    [Startdatetime] DATETIME       CONSTRAINT [DF__Log_Shrin__Start__25869641] DEFAULT (getdate()) NULL,
+    [EndDateTime]   DATETIME       NULL
+);
+
