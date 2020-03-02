797ba01229e83a445507582902f0be1024f8cba4
ravi.kanneganti <ravi.kanneganti@datascan.com>
2020-03-02 17:02:53 -0600

    dcapac path updated

--- DS_Two/Scripts/Deploy.ps1
+++ DS_Two/Scripts/Deploy.ps1
@@ -8,7 +8,7 @@ add-type -path "C:\Program Files (x86)\Microsoft SQL Server\140\DAC\bin\Microsof
 $dacService = new-object Microsoft.SqlServer.Dac.DacServices "server=redgateprod.dart.local"
 
 # Load DACPAC
-$dacpac = [Microsoft.SqlServer.Dac.DacPackage]::Load("$tcdir\bin\release\DS_Two.dacpac")
+$dacpac = [Microsoft.SqlServer.Dac.DacPackage]::Load("$tcdir\DS_Two\bin\Release\DS_Two.dacpac")
 
 #Deploy database
 $dacService.deploy($dacpac, "Stagingdb", "True")
